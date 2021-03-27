c03b71d8eaf30cc9a271a210599c454d27f937ca
keziabaidoo <keziabaidoo100@gmail.com>
2021-03-27 15:03:51 +0000

    changes made in footer and app.css

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,10 +10,14 @@ class Homepage extends Component {
     constructor(props){
         super(props);
         this.state ={
-
-            data: [{time:"5:30", task:"Learn 2 UX lessons"},]
+              
         };
     }
+
+     nextPage =()=>{
+
+     }
+
     render() {
         return (
             <div className="container">
@@ -23,7 +27,7 @@ class Homepage extends Component {
 
                 </Flex>
 
-          <List styleType="disc" margin="10" fontSize="xl" fontFamily="serif" top="10" >
+          <List styleType="disc" margin="10" fontSize="xl" fontFamily="serif" marginBottom="18" >
                 <ListItem padding="1" >Meditate</ListItem>
                 <ListItem padding="1" marginTop="2">Study for Math exam</ListItem>
                 <ListItem padding="1" marginTop="2">Sleep 8 hours</ListItem>
@@ -36,7 +40,7 @@ class Homepage extends Component {
           <div className="col">
            {/* <Flex> */}
 
-           <Box w="100%" h="660px" bgGradient="linear(to-t, pink.300, orange.200)" borderRadius="20">
+           <Box w="100%" h="660px" bgGradient="linear(to-t, pink.300, orange.200)" borderRadius="20" marginBottom="10">
                <Flex flexDirection="row" justifyContent="space-between" margin="5">  
                <Heading as="h3" size="xl" marginTop="10" marginRight="10">TASKS </Heading>
                <Text margin="10" fontWeight="bold" verticalAlign="5" fontSize="2xl">86 %</Text>
@@ -45,8 +49,8 @@ class Homepage extends Component {
                </Flex>
 
                <Flex flexDirection="row"  bg="whiteAlpha.800" borderRadius="30" h="10.5%" margin="1" position="relative">
-               <Stack direction="row" h="70px" p={0}  borderColor="orange.800" spacing="9">
-                   <Text marginLeft="3" marginTop="6">5:30</Text>
+               <Stack direction="row" h="70px" p={0}  borderColor="orange.800" spacing="7">
+                   <Text marginLeft="6" marginTop="6">5:30</Text>
                    <Divider orientation="vertical" />
                    <Text paddingTop="6">Learn 2 UX lessons</Text>
                    </Stack>
@@ -54,10 +58,10 @@ class Homepage extends Component {
 
 
                <Flex flexDirection="row"  bg="yellow.100" borderRadius="30" h="10.5%"  margin="1" marginTop="9"position="relative" >
-               <Stack direction="row" h="70px" p={0}  borderColor="orange.800" spacing="9">
+               <Stack direction="row" h="70px" p={0}  borderColor="orange.800" spacing="6">
                    <div className="col" style={{paddingTop:10,justifyContent:"center"}} >
-                   <Text>5:30</Text>
-                   <Text>10:30</Text>
+                   <Text marginLeft="6">5:30</Text>
+                   <Text marginLeft="5">10:30</Text>
                    </div>
                    <Divider orientation="vertical" />
                    <Text paddingTop="5">Marketing project at school</Text>
@@ -66,25 +70,25 @@ class Homepage extends Component {
 
 
                <Flex flexDirection="row"  bg="whiteAlpha.800" borderRadius="30" h="10.5%" margin="1" marginTop="10" >
-               <Stack direction="row" h="70px" p={0}  borderColor="orange.800"  spacing="9" >
-                   <Text paddingTop="6">17:00</Text>
+               <Stack direction="row" h="70px" p={0}  borderColor="orange.800"  spacing="6" >
+                   <Text paddingTop="6" marginLeft="5">17:00</Text>
                    <Divider orientation="vertical" />
                    <Text paddingTop="6">Buy flowers for mom</Text>
                   </Stack>
                   </Flex>
 
                <Flex flexDirection="row"  bg="whiteAlpha.800" borderRadius="30" h="10.5%" paddingTop="3.5" margin="1" marginTop="9">
-               <Stack direction="row" h="55px" p={0}  borderColor="orange.800" spacing="9">
-                <Text>17:30</Text>
+               <Stack direction="row" h="55px" p={0}  borderColor="orange.800" spacing="6">
+                <Text marginLeft="5" paddingTop="3">17:30</Text>
                 <Divider orientation="vertical" />
-                 <Text>Meditate</Text>
+                 <Text paddingTop="3">Meditate</Text>
               </Stack>
                </Flex>
 
 
                <Flex flexDirection="row"   bg="whiteAlpha.800" borderRadius="30" h="10.5%" paddingTop="3.5" margin="1" marginTop="9">
-               <Stack direction="row" h="55px" p={0}  borderColor="orange.800" spacing="9">
-                   <Text marginLeft="1" paddingTop="3">12:15</Text>
+               <Stack direction="row" h="57px" p={0}  borderColor="orange.800" spacing="6">
+                   <Text marginLeft="5" paddingTop="3">12:15</Text>
                    <Divider orientation="vertical" justify="center" />
                    <Text paddingTop="3">Shopping</Text>
  
